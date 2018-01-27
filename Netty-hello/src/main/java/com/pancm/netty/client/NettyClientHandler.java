41f1d45c79519dc35f1f24cdca83afad3232737c
xuwujing <1060589146@qq.com>
2018-01-27 18:01:29 +0800

    1.Netty第一次提交

---
+++ Netty-hello/src/main/java/com/pancm/netty/client/NettyClientHandler.java
@@ -0,0 +1,34 @@
+package com.pancm.netty.client;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * 
+* Title: NettyClientHandler
+* Description: 
+* 客户端业务逻辑实现
+* Version:1.0.0  
+* @author pancm
+* @date 2017-8-31
+ */
+public class NettyClientHandler extends SimpleChannelInboundHandler<String> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {  
+        System.out.println("客户端接受的消息: " + msg);
+    }
+
+    //
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("正在连接... ");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("连接关闭! ");
+        super.channelInactive(ctx);
+    }
+}
---
+++ Netty-hello/src/main/java/com/pancm/netty/server/NettyServerHandler.java
@@ -0,0 +1,43 @@
+package com.pancm.netty.server;
+
+import java.net.InetAddress;
+import java.util.Date;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * 
+* Title: HelloServerHandler
+* Description: 
+*  服务端业务逻辑
+* Version:1.0.0  
+* @author pancm
+* @date 2017-8-31
+ */
+public class NettyServerHandler extends SimpleChannelInboundHandler<String> {
+    /*
+     * 收到消息时，返回信息
+     */
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg)
+            throws Exception {
+        // 收到消息直接打印输出
+        System.out.println("服务端接受的消息 : " + msg);
+        if("quit".equals(msg)){//服务端断开的条件
+            ctx.close();
+        }
+        Date date=new Date();
+        // 返回客户端消息
+        ctx.writeAndFlush(date+"\n");
+    }
+
+    /*
+     * 建立连接时，返回消息
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("连接的客户端地址:" + ctx.channel().remoteAddress());
+        ctx.writeAndFlush("客户端"+ InetAddress.getLocalHost().getHostName() + "成功与服务端建立连接！ \n");
+        super.channelActive(ctx);
+    }
+}
